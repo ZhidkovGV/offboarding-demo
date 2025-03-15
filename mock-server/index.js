2b43530c9a3dda9af2df79f8611d14984223fc36
GShidkov42 <gleb@wevestr.com>
2025-03-15 14:15:08 +0100

    feat: mock server impl

---
+++ mock-server/index.js
@@ -0,0 +1,134 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+const MOCK_OFFBOARDINGS_AMOUNT = 100;
+
+const MOCK_NAMES = [
+  "John",
+  "Joanna",
+  "Jan",
+  "Victoria",
+  "Stephan",
+  "Hanna",
+  "Xi",
+  "Bartosz",
+  "Jake",
+  "Aleksandra",
+  "Sherlock",
+  "Harry",
+  "Fiona",
+];
+const MOCK_SURNAMES = [
+  "Doe",
+  "Kowalski",
+  "Grey",
+  "Black",
+  "Potter",
+  "Green",
+  "Melnik",
+  "Portnoy",
+  "Senvir",
+  "Poddar",
+  "Shrek",
+];
+
+const MOCK_DEPARTMENTS = ["Engineering", "Sales", "Design", "Marketing"];
+
+const MOCK_EQUIPMENT_NAMES = [
+  "Macbook Air",
+  "Macbook Pro",
+  "Magic Mouse",
+  "AirPods",
+  "Magic Keyboard",
+];
+
+const getRandomFromArray = (array) =>
+  array[Math.floor(Math.random() * array.length)];
+
+const getMockEquipment = (userId) =>
+  Array.from(
+    {
+      length: Math.floor(Math.random() * MOCK_EQUIPMENT_NAMES.length + 1), // add one to avoid empty equipment arrays,
+    },
+    (__, equipmentIndex) => ({
+      id: `equipment_id_${userId}_${equipmentIndex}`,
+      status: "IN_USE",
+      name: getRandomFromArray(MOCK_EQUIPMENT_NAMES),
+    }),
+  );
+
+const getRandomName = () =>
+  `${getRandomFromArray(MOCK_NAMES)} ${getRandomFromArray(MOCK_SURNAMES)}`;
+
+const getMockDB = () => {
+  return Array.from({ length: MOCK_OFFBOARDINGS_AMOUNT }, (_, i) => {
+    const name = getRandomName();
+    const equipment = getMockEquipment(i);
+
+    return {
+      employee: {
+        id: `id_${i}`,
+        name,
+        department: getRandomFromArray(MOCK_DEPARTMENTS),
+        equipment,
+        email: `${name.replace(" ", ".")}@demomail.com`,
+      },
+      status: "NOT_STARTED",
+      id: `offboarding_id_${i}`,
+    };
+  });
+};
+
+let mockDB = getMockDB();
+
+// Disable CORS
+app.use((_, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept",
+  );
+  next();
+});
+
+/**
+ * Get all offboarding processes
+ */
+app.get("/users/offboard", (_, res) => {
+  res.send(mockDB);
+});
+
+/**
+ * Get offboarding process by id
+ */
+app.get("/users/offboard/:id", (req, res) => {
+  const process = mockDB.find(req.params.id);
+  if (process) {
+    res.send(process);
+  } else {
+    res.status(404).send();
+  }
+});
+
+/**
+ * Update offboarding process entity
+ */
+app.patch("/users/offboard/:id", ({params, body}, res) => {
+  res.send({ success: true });
+});
+
+/**
+ * Complete offboarding process making it readonly
+ */
+app.post("/users/offboard/:id/complete", ({params: {id}}, res) => {
+  res.send({ success: true });
+});
+
+app.post("/reset", (_, res) => {
+  mockDB = getMockDB();
+  res.send({ success: true });
+});
+
+app.listen(PORT, () => {
+  console.log(`Mock app listening on port ${PORT}`);
+});
