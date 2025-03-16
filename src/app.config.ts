67540103a4eaa9595a37a4beef2216deb1cd09ea
ZhidkovGV <zhidkov.gleb@gmail.com>
2025-03-16 23:54:28 +0100

    feat: integrate mat moment date adapter to support dd-mm-yyyy

--- src/app.config.ts
+++ src/app.config.ts
@@ -3,12 +3,23 @@ import { provideRouter } from "@angular/router";
 
 import { routes } from "./app.routes";
 import { provideHttpClient } from "@angular/common/http";
+import { provideMomentDateAdapter } from "@angular/material-moment-adapter";
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideHttpClient(),
-    
+    provideMomentDateAdapter({
+      parse: {
+        dateInput: "DD-MM-YYYY", // Input format
+      },
+      display: {
+        dateInput: "DD-MM-YYYY", // Display format
+        monthYearLabel: "MMM YYYY",
+        dateA11yLabel: "LL",
+        monthYearA11yLabel: "MMMM YYYY",
+      },
+    }),
   ],
 };
